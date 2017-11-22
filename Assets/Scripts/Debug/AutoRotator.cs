cd0db03df1aff98dba46b104ac0ee26acfabe163
Pierre-Alexandre Leclerc <pierrealexandre.leclerc@gmail.com>
2017-11-22 15:01:33 -0500

    Debug demo in project

---
+++ Assets/Scripts/Debug/AutoRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour {
+
+     [SerializeField]
+    private Vector3 rotationSpeed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+	}
+}
